78926978e01c6711745b0930aa252fc23552ada7
Madi-S <khovansky99@gmail.com>
2021-08-06 18:15:54 +0600

    Prerequisites updated

--- project/worker.py
+++ project/worker.py
@@ -3,7 +3,6 @@ import time
 
 from celery import Celery
 
-<<<<<<< HEAD
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379')
 celery.conf.broker_url = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
@@ -11,15 +10,4 @@ celery.conf.broker_url = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localh
 @celery.task(name='create_task')
 def create_task(task_length):
     time.sleep(int(task_length) * 3)
-=======
-
-celery = Celery(__name__)
-celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
-
-
-@celery.task(name="create_task")
-def create_task(task_type):
-    time.sleep(int(task_type) * 10)
->>>>>>> 2ecc122a28b50dc08ab318486c6de0c7c95757db
     return True
